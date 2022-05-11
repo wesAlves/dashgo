8bbbf9395986977268bc9f8f4c107507f932de3d
Wes <wal2.designer@gmail.com>
2022-05-11 15:41:03 +0100

    create first login form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,61 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import {
+  Flex,
+  Input,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
 
 export default function Home() {
-  return <h1>Dashgo</h1>;
+  return (
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              focusBorderColor="pink.300"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        <Button type="submit" mt="6" colorScheme={"pink"}>
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
