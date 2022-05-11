eb1eef1f95047a8bc22bcbab3bfd328e3677367a
Wes <wal2.designer@gmail.com>
2022-05-11 16:28:39 +0100

    starting with sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,15 @@
-import { Header } from "../components/Form/Header";
+import { Flex } from "@chakra-ui/react";
+import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
   return (
-    <>
+    <Flex direction="column" h="100vh">
       <Header />
-      <div>Dashboard</div>
-    </>
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="autos" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
