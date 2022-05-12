e37d86bf4890aaeef7aa79555c93f53abc3f1854
Wes <wal2.designer@gmail.com>
2022-05-12 10:55:06 +0100

    Create user list and pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,39 @@
+import { Box, Button, Stack } from "@chakra-ui/react";
+
+export function Pagination() {
+  return (
+    <Stack
+      direction="row"
+      mt="8"
+      justify="space-between"
+      spacing="6"
+      align="center"
+    >
+      <Box>
+        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+      </Box>
+      <Stack direction="row" spacing="2">
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          colorScheme="pink"
+          disabled
+          _disabled={{ bgColor: "pink.500", cursor: "default" }}
+        >
+          1
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          colorScheme="pink"
+          bg="gray.700"
+          _hover={{ bgColor: "pink.500" }}
+        >
+          2
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
