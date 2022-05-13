5c4cc545dd47afd76ee8a8c37e7bbd0816276383
Wes <wal2.designer@gmail.com>
2022-05-13 15:22:58 +0100

    starting fetch real data

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,6 +5,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -14,12 +15,27 @@ import {
   Tr,
 } from "@chakra-ui/react";
 import Link from "next/link";
+import { useEffect } from "react";
 import { RiAddLine, RiEditLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useQuery } from "react-query";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery("users", async () => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+
+    return data;
+  });
+
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/api/users")
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // }, []);
+
   return (
     <Box>
       <Header />
@@ -44,50 +60,61 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px="6" color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Error</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px="6" color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    <Th>Data de cadastro</Th>
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Wes Benvindo</Text>
-                    <Text fontWeight="sm" color="gray.300">
-                      wes.bnvnd@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                <Td>11 May 2022</Td>
-                <Td>
-                  <Link href="/users/create" passHref>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiEditLine} />}
-                    >
-                      Editar
-                    </Button>
-                  </Link>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-
-          <Pagination />
+                <Tbody>
+                  <Tr>
+                    <Td px="6">
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Wes Benvindo</Text>
+                        <Text fontWeight="sm" color="gray.300">
+                          wes.bnvnd@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    <Td>11 May 2022</Td>
+                    <Td>
+                      <Link href="/users/create" passHref>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiEditLine} />}
+                        >
+                          Editar
+                        </Button>
+                      </Link>
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
